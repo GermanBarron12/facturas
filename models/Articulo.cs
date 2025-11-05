378bbbee675ff753bd8db396525075a2ef88e48c
German Barron <germanbarrona12@gmail.com>
2025-11-05 14:15:42 -0700

    feat: agregar clases factura y articulo

---
+++ models/Articulo.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace facturas.Models
+{
+    // Esta clase representa UN artículo dentro de una factura
+    public class Articulo
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Describe el artículo")]
+        public string Descripcion { get; set; } = "";
+
+        [Required]
+        [Range(1, 1000, ErrorMessage = "La cantidad debe ser al menos 1")]
+        public int Cantidad { get; set; } = 1;
+
+        [Required]
+        [Range(0.01, 999999, ErrorMessage = "El precio debe ser mayor a 0")]
+        public decimal Precio { get; set; }
+
+        // A qué factura pertenece este artículo
+        public int FacturaId { get; set; }
+    }
+}
---
+++ models/Factura.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace facturas.Models
+{
+    // Esta es la clase principal - representa UNA factura
+    public class Factura
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Pon la fecha")]
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "Pon el nombre del cliente")]
+        public string NombreCliente { get; set; } = "";
+
+        public decimal Total { get; set; }
+
+        // Una factura puede tener muchos artículos
+        public List<Articulo> Articulos { get; set; } = new List<Articulo>();
+    }
+}
